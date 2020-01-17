a9dc08919b18f224f6b50af62304be53a984935a
Jan Weidner <jw3126@gmail.com>
2020-01-17 07:46:15 -0800

    init

--- src/MagicMarshalling.jl
+++ src/MagicMarshalling.jl
@@ -1,5 +1,94 @@
 module MagicMarshalling
 
-greet() = print("Hello World!")
+export marshal, unmarshal
+
+using ArgCheck: @check, @argcheck
+using ConstructionBase: constructorof, ConstructionBase
+
+KEY_META = "__meta__"
+KEY_CONSTRUCTOR = "constructor"
+KEY_PROPERTYNAMES = "propertynames"
+PKGNAME = "MagicMarshalling"
+
+function __init__()
+    # THIS IS HACKY
+    # We need it so that ConstructionBase is visible in Main
+    Core.eval(Main, :(using ConstructionBase: ConstructionBase))
+end
+
+constructor_string(o) = string(constructorof(typeof(o)))
+constructor_string(::Tuple) = string(tuple)
+constructor_string(o::Array) = string(reshape)
+
+unmarshal(o::Vector) = map(unmarshal, o)
+function marshal(o::Array)
+    d = Dict{String, Any}()
+    meta = Dict(
+        KEY_CONSTRUCTOR => constructor_string(o),
+        KEY_PROPERTYNAMES => ["data", "size"], # eltype?
+    )
+    d[KEY_META] = meta
+    d["data"] = map(marshal, vec(o))
+    d["size"] = marshal(size(o))
+    d
+end
+
+function marshal_struct(o)
+    d = Dict{String, Any}()
+    meta = Dict(
+        KEY_CONSTRUCTOR => constructor_string(o),
+        KEY_PROPERTYNAMES => collect(map(string, propertynames(o))),
+    )
+    d[KEY_META] = meta
+    for prop in propertynames(o)
+        key = string(prop)
+        d[key] = marshal(getproperty(o, prop))
+    end
+    d
+end
+
+marshal(o) = marshal_struct(o)
+
+SelfMarshalTypes = Union{
+    String, Int, Float64,
+    }
+marshal(o::SelfMarshalTypes) = o
+unmarshal(o::SelfMarshalTypes) = o
+
+function unmarshal(d::Dict)
+    @argcheck haskey(d, KEY_META)
+    meta = d[KEY_META]
+    @check haskey(meta, KEY_CONSTRUCTOR)
+    @check haskey(meta, KEY_PROPERTYNAMES)
+
+    ctor_str = meta[KEY_CONSTRUCTOR]
+    @check ctor_str isa String
+    ctor_expr = nothing
+    try
+        ctor_expr = Meta.parse(ctor_str)
+    catch err
+        msg = """Error parsing constructor:
+        string: $ctor_str
+        error: $err
+        """
+        error(msg)
+    end
+    ctor = nothing
+    try
+        ctor = Core.eval(Main, ctor_expr)
+    catch err
+        msg = """Error evaluating constructor expression:
+        string: $ctor_str
+        expr: $ctor_expr
+        error: $err
+        """
+        error(msg)
+    end
+    propnames = meta[KEY_PROPERTYNAMES]
+    args = map(propnames) do key
+        unmarshal(d[key])
+    end
+    ctor(args...)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,78 @@
 using MagicMarshalling
 using Test
 
-@testset "MagicMarshalling.jl" begin
-    # Write your own tests here.
+is_simple_type(o) = false
+is_simple_type(o::Int) = true
+is_simple_type(o::Float64) = true
+is_simple_type(o::String) = true
+
+function is_simple_type(o::Vector)
+    for x in o
+        is_simple_type(x) || return false
+    end
+    return true
+end
+function is_simple_type(o::Dict)
+    for (k,v) in pairs(o)
+        k isa String || return false
+        is_simple_type(v) || return false
+    end
+    return true
+end
+
+@testset "Basic bitstypes" begin
+    for o in [1,1.0,-2,NaN,-Inf,]
+        m = marshal(o)
+        @test is_simple_type(m)
+        o2 = unmarshal(m)
+        @test o === o2
+    end
+end
+
+@testset "NamedTuple" begin
+    o = (a=1, b=2)
+    m = marshal(o)
+    @test is_simple_type(m)
+    o2 = unmarshal(m)
+    @test o === o2
+end
+
+@testset "Tuple $o" for o in [(), (1,), (1,2), ((1,2), (3,4))]
+    m = marshal(o)
+    @test is_simple_type(m)
+    o2 = unmarshal(m)
+    @test o === o2
+end
+
+@testset "Array" begin
+    for o in [
+                [1,2],
+                [1 2; 3 4],
+                ["a", 2],
+                [],
+                [[1]],
+                [[1], [1,[3]]],
+        ]
+        m = marshal(o)
+        @test is_simple_type(m)
+        o2 = unmarshal(m)
+        @test o == o2
+    end
+end
+
+@testset "Structs" begin
+    struct ParametricStruct{A,B}
+        a::A
+        b::B
+    end
+
+    for o in [
+        ParametricStruct(1,2),
+        ParametricStruct(ParametricStruct(1,2), ParametricStruct(1.0, 3)),
+       ]
+        m = marshal(o)
+        @test is_simple_type(m)
+        o2 = unmarshal(m)
+        @test o === o2
+    end
 end
